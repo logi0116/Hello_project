dc7ceedc7173579e7f8e8f5aa04f686115ae2cfb
logi0116 <roguewrite001@gmail.com>
2025-06-24 14:09:16 +0900

    수정

---
+++ src/main/java/com/busanit501/hello_project/domain/TodoVO.java
@@ -0,0 +1,19 @@
+package com.busanit501.hello_project.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+// 소스코드 생성이 아니라.
+// 롬복 이용해서, 사용해보기.
+@Getter
+@ToString
+@Builder
+public class TodoVO {
+    private Long tno;
+    private String title;
+    private LocalDate dueDate;
+    private boolean finished;
+}
